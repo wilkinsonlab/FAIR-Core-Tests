336accef8706397036c5eca955103a93ee5fd35e
Mark Wilkinson <markw@illuminae.com>
2024-03-28 17:04:53 +0100

    spacing and stuff

--- app/controllers/routes.rb
+++ app/controllers/routes.rb
@@ -2,47 +2,45 @@
 
 def set_routes(classes: allclasses)
   set :server_settings, timeout: 180
-  set :public_folder, "public"
+  set :public_folder, 'public'
 
-  get "/" do
+  get '/' do
     redirect '/tests/list'
   end
 
-  get "/tests/" do
+  get '/tests/' do
     content_type :json
     response.body = JSON.dump(Swagger::Blocks.build_root_json(classes))
   end
 
-  get "/tests/list" do
-
+  get '/tests/list' do
     erb :listtests
   end
-  post "/tests/:id" do
+  post '/tests/:id' do
+    content_type :json
     id = params[:id]
     payload = JSON.parse(request.body.read)
-    guid = payload["subject"]
-    #begin
-      @result = FAIRTest.send(id,**{guid: guid})
-    #rescue
-    #  @result = "{}"
-    #end
+    guid = payload['subject']
+    begin
+      @result = FAIRTest.send(id, **{ guid: guid })
+    rescue StandardError
+      @result = '{}'
+    end
     @result.to_json
   end
 
-  get "/tests/:id" do
+  get '/tests/:id' do
+    content_type 'application/openapi+yaml'
     id = params[:id]
-    id += "_api"
-    #begin
+    id += '_api'
+    begin
       @result = FAIRTest.send(id)
-
-    #rescue
-    #  @result = "{}"
-    #end
+    rescue StandardError
+      @result = ''
+    end
     @result
   end
 
-
   before do
-#
   end
 end
