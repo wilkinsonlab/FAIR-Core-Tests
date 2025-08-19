b2d7dd2a8ece2ad464f125d3233f6ea275975f62
Mark Wilkinson <markw@illuminae.com>
2025-08-19 08:57:23 +0200

    fixed the application domain and digital object type predicates

--- app/lib/dcat_metadata.rb
+++ app/lib/dcat_metadata.rb
@@ -3,7 +3,7 @@ module ChampionDCAT
     attr_accessor :identifier, :testname, :description, :keywords, :creator,
                   :indicators, :end_desc, :end_url, :dctype, :testid, :supportedby,
                   :license, :themes, :testversion, :implementations, :isapplicablefor, :applicationarea,
-                  :organizations, :individuals, :protocol, :host, :basePath, :metric, :dotype
+                  :organizations, :individuals, :protocol, :host, :basePath, :metric
     require_rel './output.rb'
 
     def initialize(meta:)
@@ -20,8 +20,8 @@ module ChampionDCAT
       @end_url = meta[:end_url]
       @dctype = meta[:dctype] || "http://edamontology.org/operation_2428"
       @supportedby = meta[:supportedby] || ["https://tools.ostrails.eu/champion"]
-      @isapplicablefor = meta[:applicationarea] || ["http://www.fairsharing.org/ontology/subject/SRAO_0000401"]
-      @dotype = meta[:isapplicablefor] || ["https://schema.org/Dataset"]
+      @applicationarea = meta[:applicationarea] || ["http://www.fairsharing.org/ontology/subject/SRAO_0000401"]
+      @isapplicablefor = meta[:isapplicablefor] || ["https://schema.org/Dataset"]
       @license = meta[:license]
       @themes = meta[:themes]
       @themes = [@themes] unless @themes.is_a? Array
@@ -147,14 +147,14 @@ module ChampionDCAT
 
       supportedby.each do |tool|
         FAIRChampion::Output.triplify(me, ftr.supportedBy, tool,  g)
-        FAIRChampion::Output.triplify(tool, RDF.type, schema.SoftwareApplication,  g)
+        FAIRChampion::Output.triplify(tool, RDF.type, schema.SoftwareApplication, g)
       end
 
-      isapplicablefor.each do |domain|
-        FAIRChampion::Output.triplify(me, dpv.isApplicableFor, domain, g)
+      applicationarea.each do |domain|
+        FAIRChampion::Output.triplify(me, ftr.applicationArea, domain, g)
       end
-      dotype.each do |digitalo|
-        FAIRChampion::Output.triplify(me, ftr.applicationArea, digitalo, g)
+      isapplicablefor.each do |digitalo|
+        FAIRChampion::Output.triplify(me, dpv.isApplicableFor, digitalo, g)
       end
 
 
