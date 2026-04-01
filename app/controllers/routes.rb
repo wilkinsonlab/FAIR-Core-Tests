7537949c15c1d91c07d5aca970e63145b4513e55
markwilkinson <mark.wilkinson@upm.es>
2026-04-01 09:58:41 +0200

    basepath needed for web view of test

--- app/controllers/routes.rb
+++ app/controllers/routes.rb
@@ -24,9 +24,9 @@ def set_routes(classes: [])
     @tests = ts.map { |t| t.match(%r{.*/(\S+)\.rb$})[1] } # This is just the final field in the URL
     # def initialize(test_host:, basepath:, test_protocol:)
     infra = FtrRuby::TestInfra.new(test_host: test_host, basepath: basepath, test_protocol: test_protocol)
-
+    @basepath = basepath
     @labels, @lps = infra.get_tests_metrics(tests: @tests) # the local URL is built in this routine, and called
-    warn "landing pages #{@lps.inspect}"
+    # warn "landing pages #{@lps.inspect}"
     halt erb :listtests, layout: :listtests_layout
   end
 
